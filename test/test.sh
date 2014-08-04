a9a12dfa883354708f6d8c86e02f1901ce1cd849
Martin Panter <vadmium  gmailcom>
2014-08-04 07:05:38 +0000

    Allow list of Python versions to be specified

--- test/test.sh
+++ test/test.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-shopt -s expand_aliases
-type python2 >/dev/null 2>&1 || alias python2='python'
+export PYTHON2=python2
+type "$PYTHON2" >/dev/null 2>&1 || PYTHON2=python
+
+if test "$#" -eq 0; then
+    set -- "$PYTHON2" python3
+fi
 
 echo "Starting proxy servers..."
 python2 socks4server.py > /dev/null &
 python2 httpproxy.py > /dev/null &
 ./mocks start
 
-sleep 2
-echo "Python 2.x tests"
-python2 sockstest.py -v
-
-sleep 2
-echo "Python 3.x tests"
-python3 sockstest.py -v
+for PYTHON; do
+    sleep 2
+    echo "$PYTHON tests"
+    "$PYTHON" sockstest.py -v
+done
 
 pkill python2 > /dev/null
 ./mocks shutdown
